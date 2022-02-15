dd15f2e9dd5f6717336c53926adcc5a56b33d592
Reaper47i <vineetbhengra96@gmail.com>
2022-02-15 19:44:38 +0530

    new features 😁

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
+const projects = [
+  {
+    id : '1',
+    name:'Github Finder',
+    description:'Github Finder App that uses Github API to find first 300 profile names and info.'
+  },
+  {
+    id:'2'
+  }
+]
+
 const Projects = () => {
   return (
-    <div className='projects' id='projects'>Projects</div>
+    <div className='projects' id='projects'>
+      <div className="pj-box">
+        <h1>Projects</h1>
+        <div className="pj-content">
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -21,13 +21,16 @@ const tools = [
 const Skills = () => {
   return (
     <div className='skills' id='skills'>
-      <div className="Skcontent">
-          <div className='sk-h'><h3>Below are some my skills and technologies I have worked on</h3></div>
-          <span className='line3 sp'></span>
-          <div> <h3>Languages</h3> {languages.map((items)=> <div key={items}>{items}</div>)} </div>
-          <div> <h3>Frameworks</h3> {frameworks.map((f)=> <div key={f}>{f}</div>)} </div>
-          <div> <h3>Other Technologies</h3> {tools.map((t)=> <div key={t}>{t}</div>)} </div>
+      <div className="sk-box">
+        <div className="Skcontent">
+            <div className='sk-h'><h3>Below are some my skills and technologies I have worked on</h3></div>
+            <span className='line3 sp'></span>
+            <div> <h3>Languages</h3> {languages.map((items)=> <div key={items}>{items}</div>)} </div>
+            <div> <h3>Frameworks</h3> {frameworks.map((f)=> <div key={f}>{f}</div>)} </div>
+            <div> <h3>Other Technologies</h3> {tools.map((t)=> <div key={t}>{t}</div>)} </div>
+        </div>
       </div>
+      <div className="wave3"></div>
     </div>
   )
 }
@@ -35,10 +38,3 @@ const Skills = () => {
 export default Skills
 
 
-
-/**
- * programming languages
- * front-end framework
- * UI tools
- * 
- */
